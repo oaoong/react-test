057db47bc9628d44c5f15635307f08d1a65d6bac
정재희 <pizzachicken@kakao.com>
2022-08-12 17:49:58 +0900

    counter

--- react-test-app/src/App.test.js
+++ react-test-app/src/App.test.js
@@ -1,8 +1,23 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("the counter starts at 0", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  //screen object를 이용해서 원하는 엘리먼트에 접근(접근 시 ID로)
+  const counterElement = screen.getByTestId("counter");
+  // id가 counter인 엘리먼트의 텍스트가 0인지 테스트
+  expect(counterElement).toHaveTextContent(0);
+});
+
+test("minus button has correct text", () => {
+  // App 컴포넌트를 렌더링
+  render(<App />);
+  const minusButtonElement = screen.getByTestId("minus-button");
+  expect(minusButtonElement).toHaveTextContent("-");
+});
+
+test("plus button has correct text", () => {
+  render(<App />);
+  const plusButtonElement = screen.getByTestId("plus-button");
+  expect(plusButtonElement).toHaveTextContent("+");
 });
