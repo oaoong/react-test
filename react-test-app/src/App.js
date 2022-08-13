ed681135165060e2fc1677a9dc3779243606c614
정재희 <pizzachicken@kakao.com>
2022-08-13 14:18:14 +0900

    simple test app

--- react-test-app/src/App.js
+++ react-test-app/src/App.js
@@ -3,13 +3,35 @@ import { useState } from "react";
 
 function App() {
   const [counter, setcounter] = useState(0);
+  const [disabled, setdisabled] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
         <h3 data-testid="counter">{counter}</h3>
         <div>
-          <button data-testid="minus-button">-</button>
-          <button data-testid="plus-button">+</button>
+          <button
+            data-testid="minus-button"
+            onClick={() => setcounter((count) => count - 1)}
+            disabled={disabled}
+          >
+            -
+          </button>
+          <button
+            data-testid="plus-button"
+            onClick={() => setcounter((count) => count + 1)}
+            disabled={disabled}
+          >
+            +
+          </button>
+        </div>
+        <div>
+          <button
+            data-testid="on/off-button"
+            style={{ backgroundColor: "blue" }}
+            onClick={() => setdisabled((prev) => !prev)}
+          >
+            on/off
+          </button>
         </div>
       </header>
     </div>
