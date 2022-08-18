22c9ebb83344fffbd130f0937f24391061800539
정재희 <pizzachicken@kakao.com>
2022-08-18 23:32:34 +0900

    react context

--- react-shop-test/src/test-utils.js
+++ react-shop-test/src/test-utils.js
@@ -1,9 +1,10 @@
+//test-utils.js
 import { render } from "@testing-library/react";
 import { OrderContextProvider } from "./contexts/OrderContext";
 
-const customRender = (ui, options) => 
-    render(ui, {wrapper: OrderContextProvider, ...options})
+const customRender = (ui, options) =>
+  render(ui, { wrapper: OrderContextProvider, ...options });
 
-export * from '@testing-library/react'
+export * from "@testing-library/react";
 
-export {customRender as render}
+export { customRender as render };
